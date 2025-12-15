eae2b09a5ac74631e59dbf9322fb29014933af83
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-12-15 19:32:06 +0530

    Utils upload route setup

--- services/utils/dist/index.js
+++ services/utils/dist/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import utilRoutes from "./routes/utils.route.js";
 dotenv.config();
 const app = express();
-app.use(express.json());
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use("/api/utils", utilRoutes);
 app.listen(process.env.PORT, () => {
     console.log(`Utils service is running on http://localhost:${process.env.PORT}`);
 });
